93038e12d12483eddf9aed9e67127582b59381d6
darri <iaroslag@metropolia.fi>
2023-11-04 16:40:37 +0200

    täytelistat muokattu, nuoilt korjattu, effektit

--- pizza/pizza.js
+++ pizza/pizza.js
@@ -1,32 +1,6 @@
-const categoryTitles = document.querySelectorAll('.category-title');
-
-    categoryTitles.forEach(function (title) {
-        title.addEventListener('click', function () {
-            const content = title.nextElementSibling;
-            const arrow = title.querySelector('.arrow');
-
-            if (content.style.display === 'block' || content.style.display === '') {
-                content.style.display = 'none';
-                content.style.maxHeight = '0';
-                arrow.style.transform = 'rotate(0deg)';
-            } else {
-                const openCategories = document.querySelectorAll('.category-content');
-                openCategories.forEach(function (openCategory) {
-                    openCategory.style.display = 'none';
-                    openCategory.style.maxHeight = '0';
-                });
-
-                content.style.display = 'block';
-                content.style.maxHeight = 'none';
-                arrow.style.transform = 'rotate(180deg)';
-            }
-        });
-    });
-
     const doughButtons = document.querySelectorAll('.dough button');
     const sizeButtons = document.querySelectorAll('.size button');
 
-    // Add event listeners to dough buttons
     doughButtons.forEach(button => {
         button.addEventListener('click', function() {
             this.style.border = 'solid 3px green';
@@ -35,7 +9,6 @@ const categoryTitles = document.querySelectorAll('.category-title');
         });
     });
 
-    // Add event listeners to size buttons
     sizeButtons.forEach(button => {
         button.addEventListener('click', function () {
             this.style.fontWeight = '750';
@@ -45,3 +18,32 @@ const categoryTitles = document.querySelectorAll('.category-title');
     });
 
 
+const categoryTitles = document.querySelectorAll('.category-title');
+
+categoryTitles.forEach(function (title) {
+    title.addEventListener('click', function () {
+        const content = title.nextElementSibling;
+        const arrow = title.querySelector('.arrow');
+
+        if (content.style.display === 'block' || content.style.display === '') {
+            content.style.display = 'none';
+            content.style.maxHeight = '0';
+            arrow.style.transform = 'rotate(0deg)';
+        } else {
+            const openCategories = document.querySelectorAll('.category-content');
+            openCategories.forEach(function (openCategory) {
+                openCategory.style.display = 'none';
+                openCategory.style.maxHeight = '0';
+
+                const arrowToReset = openCategory.previousElementSibling.querySelector('.arrow');
+                arrowToReset.style.transform = 'rotate(0deg)';
+            });
+
+            content.style.display = 'block';
+            content.style.maxHeight = content.scrollHeight + 'px'; 
+            arrow.style.transform = 'rotate(180deg)';
+        }
+    });
+});
+
+    
