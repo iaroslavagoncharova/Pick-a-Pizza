48f454d7f07c01bc944bec40b7c580157936f6b8
JuanFRosales <Juan.RosalesMakinen@metropolia.fi>
2023-11-14 16:36:46 +0200

    mock data

--- database/database.sql
+++ database/database.sql
@@ -98,68 +98,116 @@ CREATE TABLE PizzaIngredient (
     FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
     FOREIGN KEY (ingredient_id) REFERENCES Ingredients(ingredient_id)
 );
+ 
 
--- INSERT INTO Users (username, password, email, address, favorite_pizza, phone_number, created_at) 
--- VALUES ('test1', '12345', 'test1@gmail.com', 'Esimerkkikatu 1, Helsinki', 'gluten-free tuna', '123456789', '2023-11-14 12:48:00');
-
--- INSERT INTO ShoppingCart (price, user_id) 
--- VALUES (10.00, 1), (15.00, 1), (20.00, 1);
-
-
--- INSERT INTO Orders (cart_id, order_status, user_id) 
--- VALUES (2, 'in_progress', 1), (3, 'in_progress', 1), (4, 'in_progress', 1), (5, 'in_progress', 1);
+-- User data
+INSERT INTO Users (username, password, email, address, favorite_pizza, phone_number, created_at) 
+VALUES
+('Anna', 'password123', 'anna@example.com', 'Karaportti 2, Espoo', 'Classic', '123456789', '2023-11-14 12:48:00'),
+('Slava', 'securepass', 'slava@example.com', 'Karaportti 2, Espoo', 'Low Calorie', '987654321', '2023-11-14 13:30:00'),
+('Juan', 'pass123', 'juan@example.com', 'Karaportti 2, Espoo' , 'Keto', '555666777', '2023-11-14 14:15:00');
 
 
--- INSERT INTO Orders (cart_id, order_status, user_id) 
--- VALUES ()
+-- Add data for prompts
+INSERT INTO Prompts (prompt_name, dough, size, price, calories, carbs, protein, fats) 
+VALUES
+('Gluten Free', 'gluten-free', 'M', 10.00, 500, 50, 50, 50),
+('Keto', 'usual', 'L', 15.00, 600, 60, 60, 60),
+('Low Calorie', 'usual', 'M', 12.00, 400, 40, 40, 40),
+('Classic', 'usual', 'L', 14.00, 700, 70, 70, 70),
+('Vegan Classic', 'vegan', 'M', 13.00, 550, 55, 55, 55),
+('Gluten Free Classic', 'gluten-free', 'L', 16.00, 600, 60, 60, 60),
+('Season Deal', 'usual', 'L', 18.00, 800, 80, 80, 80);
 
--- INSERT INTO Reviews (review_text, stars, user_id) 
--- VALUES (
---     'I love this place! I order pizza here every week and it always arrives hot and delicious. I recommend it to everyone!', 
---     5, 
---     1
--- );
-
--- INSERT INTO Reviews (review_text, stars, user_id) 
--- VALUES (
---     'I ordered a pizza and it was cold and soggy. I will never order from here again.', 
---     1, 
---     1
--- );
-
--- INSERT INTO Prompts (prompt_name, dough, size, price, calories, carbs, protein, fats) 
--- VALUES (
---     'gluten-free', 'gluten-free', 'M', 10.00, 500, 50, 50, 50
--- ), (
---     'vegan', 'usual', 'L', 15.00, 600, 60, 60, 60
--- );
-
--- INSERT INTO Ingredients (name, portion_size, price, calories, carbs, protein, fats, in_stock) 
--- VALUES (
---     'tomato', 100, 1.00, 100, 10, 10, 10, 1000
--- ),
--- ('tuna', 100, 3.00, 200, 20, 20, 20, 1000);
+-- Add Ingredient data
+INSERT INTO Ingredients (name, portion_size, price, calories, carbs, protein, fats, in_stock) 
+VALUES
+('chicken', 100, 2.50, 200, 0, 30, 10, 1000),
+('beef', 100, 3.00, 250, 0, 26, 15, 1000),
+('bacon', 50, 1.50, 120, 0, 10, 8, 500),
+('ham', 50, 2.00, 150, 2, 15, 10, 500),
+('tomatoes', 50, 0.75, 20, 5, 1, 0, 1000),
+('onions', 50, 0.50, 30, 7, 1, 0, 1000),
+('eggplants', 50, 1.00, 40, 10, 2, 0, 1000),
+('mushrooms', 50, 1.20, 15, 3, 2, 0, 1000),
+('mozzarella', 50, 1.50, 200, 1, 10, 15, 1000),
+('parmezan', 50, 2.00, 180, 0, 15, 13, 1000),
+('gouda', 50, 1.75, 160, 1, 12, 10, 1000),
+('goat-cheese', 50, 2.50, 120, 0, 8, 10, 1000),
+('tofu', 50, 2.00, 150, 3, 10, 8, 1000),
+('vege-gouda', 50, 2.00, 160, 2, 10, 8, 1000),
+('anchovies', 50, 2.50, 100, 0, 15, 5, 500),
+('shrimps', 50, 3.00, 120, 1, 18, 5, 500),
+('mussels', 50, 2.50, 100, 2, 15, 3, 500),
+('tomato-sauce', 50, 1.00, 30, 7, 1, 0, 1000),
+('pesto-sauce', 50, 2.00, 80, 2, 2, 7, 1000),
+('bbq-sauce', 50, 1.50, 50, 10, 0, 0, 1000);
 
 
--- INSERT INTO PromptIngredient (prompt_id, ingredient_id) 
--- VALUES 
--- (1, 1),
--- (1, 2);
 
--- INSERT INTO Pizza (dough, size, message, calories, carbs, protein, fats, price, prompt_id) 
--- VALUES (
---     'gluten-free', 'M', 'I would like it to be extra hot if possible', 500, 50, 50, 50, 10.00, 1
--- ), (
---     'usual', 'L', '', 600, 60, 60, 60, 15.00, 2
--- );
 
--- INSERT INTO CartPizza (cart_id, pizza_id) 
--- VALUES (
---     2, 1
--- ), (
---     2, 2
--- );
+-- Example: 'Gluten Free' prompt
+INSERT INTO PromptIngredient (prompt_id, ingredient_id) 
+VALUES
+(8, 5), -- tomatoes
+(8, 6), -- onions
+(8, 7); -- eggplants
 
+-- Example: 'Keto' prompt
+INSERT INTO PromptIngredient (prompt_id, ingredient_id) 
+VALUES
+(9, 1), -- chicken
+(9, 2), -- beef
+(9, 3), -- bacon
+
+
+
+-- Example: 'Vegan Classic' prompt
+INSERT INTO PromptIngredient (prompt_id, ingredient_id) 
+VALUES
+(12, 5), -- tomatoes
+(12, 6), -- onions
+(12, 7), -- eggplants
+(12, 10), -- mozzarella
+(12, 12); -- gouda
+
+
+
+-- Add data to Pizza 
+INSERT INTO Pizza (dough, size, message, calories, carbs, protein, fats, price, prompt_id) 
+VALUES
+('usual', 'L', 'Extra cheese, please!', 700, 70, 70, 70, 14.00, 11), -- Classic pizza for Anna
+('usual', 'M', 'No onions, extra mushrooms.', 400, 40, 40, 40, 12.00, 10), -- Low Calorie pizza for Slava
+('keto', 'L', 'Keto supreme pizza!', 800, 80, 80, 80, 18.00, 9); -- Keto pizza for Juan
+
+
+-- Add data to ShoppingCart 
+INSERT INTO ShoppingCart (price, user_id) 
+VALUES
+(20.00, 1), -- User Anna
+(15.00, 2), -- User Slava
+(18.00, 3); -- User Juan
+
+-- Add data to CartPizza 
+INSERT INTO CartPizza (cart_id, pizza_id) 
+VALUES
+(1, 7), -- Anna's order, completed 
+(2, 8), -- Slava's order, in progress
+(3, 9); -- Juan's order, completed 
+
+-- Add data to Orders
+INSERT INTO Orders (cart_id, order_status, user_id) 
+VALUES
+(1, 'completed', 1), -- Order for Anna
+(2, 'in_progress', 2), -- Order in progress for Slava
+(3, 'completed', 3); -- Order for Juan
+
+-- Add data to Reviews 
+INSERT INTO Reviews (review_text, stars, user_id) 
+VALUES
+('Great pizza! Loved the gluten-free option.', 5, 1), -- Review by Anna
+('The low-calorie pizza was amazing.', 4, 2), -- Review by Slava
+('Keto pizza was awesome! Will order again.', 5, 3); -- Review by Juan
 
 -- Update order status when it's ready:
 
