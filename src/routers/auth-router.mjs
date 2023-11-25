b1ee2c4f3e89eb9686316f0083cfee93aba98a49
darri <iaroslag@metropolia.fi>
2023-11-25 21:06:06 +0200

    logo, reitti, profiili, ostoskori toimii

--- src/routers/auth-router.mjs
+++ src/routers/auth-router.mjs
@@ -9,17 +9,17 @@ const authRouter = express.Router();
 
 // TODO: post request with appropriate function
 authRouter.get('/sign-in', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/login-register', 'login-register.html'));
+  res.sendFile(path.join(__dirname, '../../public/login-register/login-register.html'));
 });
 
 authRouter.get('/my-account/:id', (req, res) => {
   const userId = req.params.id;
-  res.sendFile(path.join(__dirname, '../../public/profile', 'profile.html'));
+  res.sendFile(path.join(__dirname, '../../public/profile/profile.html'));
 })
 
 authRouter.get('/admin/:id', (req, res) => {
   const userId = req.params.id;
-  res.sendFile(path.join(__dirname, '../../public/admin-profile', 'admin-profile.html'));
+  res.sendFile(path.join(__dirname, '../../public/admin-profile/admin-profile.html'));
   // TODO: check user level id, if it corresponds to an administration role, direct to this page
 });
 
