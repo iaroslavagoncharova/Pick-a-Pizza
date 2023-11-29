ee79afb2a65ff2c9556a850c6ae37b20719c476d
mannals <annamalassu@gmail.com>
2023-11-29 13:18:18 +0200

    nyt sisäänkirjautuminen onnistuu

--- src/controllers/auth-controller.mjs
+++ src/controllers/auth-controller.mjs
@@ -1,13 +1,27 @@
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
-import { loginUser } from "../models/user-model.mjs";
+import loginUser from "../models/user-model.mjs";
 
-const postLogin = async (req, res) => {
+const postLogin = async (req, res, next) => {
+    console.log('postLogin', req.body);
     const user = await loginUser(req.body);
+
+    if (!user) {
+        const error = new Error('invalid username/password');
+        error.status = 401;
+        return next(error);
+    }
+
+    if (user.error) {
+        const error = new Error(user.error);
+        error.status = 401;
+        return next(error);
+    }
+
     console.log('postLogin', user);
     try {
         const token = jwt.sign(user, process.env.JWT_SECRET);
-        res.json({message: 'logged in', token, user});
+        res.json({message: 'logged in', token: token, user: user});
     } catch (e) {
         res.status(401).json({message: 'invalid username/password'});
     }
