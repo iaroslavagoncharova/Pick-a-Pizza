caecad76fefd3af08216b01881cd879d794c9660
mannals <annamalassu@gmail.com>
2023-12-08 21:31:50 +0200

    nyt voi muokata keskeneräisiä tilauksia valmiiksi ja poistaa niitä

--- public/admin-profile/admin-profile.js
+++ public/admin-profile/admin-profile.js
@@ -24,11 +24,6 @@ const ordersBtn = document.getElementById('all-orders');
 const dialog = document.getElementById('edit-profile-dialog');
 const closeDialog = document.getElementById('close-dialog');
 
-const deleteBtn = document.getElementById('delete-btn');
-const deletePopup = document.getElementById('confirm-deletion');
-const cancelBtn = document.getElementById('cancel');
-const confirmBtn = document.getElementById('confirm');
-
 const header = document.querySelector('header');
 const pageContent = document.querySelector('.hero-image');
 const footer = document.querySelector('footer');
@@ -47,4 +42,4 @@ closeDialog.addEventListener('click', () => {
     header.classList.remove('blur-background');
     pageContent.classList.remove('blur-background');
     footer.classList.remove('blur-background');
-})
+});
--- src/routers/order-router.mjs
+++ src/routers/order-router.mjs
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getOrderHistoryOfUser, getOrdersInProgress } from '../controllers/orders-controller.mjs';
+import { getOrderHistoryOfUser, getOrdersInProgress, removeOrder, setOrderAsCompleted } from '../controllers/orders-controller.mjs';
 
 const orderRouter = express.Router();
 
 orderRouter.route('/:id').get(getOrderHistoryOfUser);
-orderRouter.route('/wip/auth/:id').get(getOrdersInProgress);
+orderRouter.route('/wip/auth/:id').get(getOrdersInProgress).put(setOrderAsCompleted).delete(removeOrder);
 
 export default orderRouter;
